1eb36d0f0717fa1dfb145d63ed0ea8dc6d93d628
José Luis Reyes <joseluis.r85@gmail.com>
2020-12-01 00:52:42 -0600

    iniciando con el proyecto

---
+++ BackEnd/src/index.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+
+// iniciar server
+const main = () => {
+  app.listen(app.get('PORT'), () => {
+    console.log(`servidor andando en: ${app.get("PORT")}`);
+  });
+};
+
+main();
