265ab540d54bd70c14983b97498020d903d962f6
José Luis Reyes <joseluis.r85@gmail.com>
2020-12-14 23:01:18 -0600

    agregando create

--- BackEnd/src/controllers/tarjetas.controller.js
+++ BackEnd/src/controllers/tarjetas.controller.js
@@ -1,24 +1,66 @@
 const db = require("../models/index");
 const tarjetas = db.tarjetas;
 const infoTarjeta = db.infoTarjeta;
+const redesTarjeta = db.redesTarjeta;
 
 exports.findAll = async (req, res) => {};
 
 exports.findOne = async (req, res) => {};
 
 exports.create = async (req, res) => {
+  //console.log(req.body);
+
   let infoTarjetaBody = req.body.infoTarjeta;
+  var tarjetaBody;
 
-  infoTarjeta.create(infoTarjetaBody).then((data) => {
-    console.log(data);
+  if (infoTarjetaBody.idInfoTarjeta == undefined) {
+    infoTarjetaBody.idUsuario = req.body.idUsuario;
 
-    if(data){
-      
+    //console.log(infoTarjetaBody);
+
+    await infoTarjeta.create(infoTarjetaBody).then((data) => {
+      //console.log(data.idInfoTarjeta);
+      if (data.idInfoTarjeta) {
+        infoTarjetaBody.idInfoTarjeta = data.idInfoTarjeta;
+      }
+    });
+  }
+
+  let tarjetaGenerada;
+
+  tarjetaBody = {
+    nombreTarjeta: req.body.nombreTarjeta,
+    description: req.body.description,
+    idUsuario: req.body.idUsuario,
+    idEstilo: req.body.idEstilo,
+    idInfoTarjeta: infoTarjetaBody.idInfoTarjeta,
+    idEstado: 1,
+  };
+
+  var idTarjetaInfoGenerated;
+
+  await tarjetas.create(tarjetaBody).then((data) => {
+    tarjetaGenerada = data;
+    if (data) {
+      idTarjetaInfoGenerated = data.idTarjeta;
     }
-
   });
 
-  await res.send(req.body);
+  console.log(idTarjetaInfoGenerated);
+
+  for (const item in req.body.redesTarjeta) {
+    let values = {
+      item: req.body.redesTarjeta[item],
+    };
+
+    values.item.idTarjeta = idTarjetaInfoGenerated;
+
+    redesTarjeta.create(values.item).then((data) => {
+      console.log("redes guardadas");
+    });
+  }
+
+  await res.send(tarjetaGenerada);
 };
 
 exports.update = async (req, res) => {};
