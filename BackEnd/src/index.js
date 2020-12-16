43dfcf667fe988ad9886e1f61748890d92844bf0
José Luis Reyes <joseluis.r85@gmail.com>
2020-12-15 22:17:14 -0600

    agergando polyfill

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,3 +1,4 @@
+import "@babel/polyfill";
 const app = require("./app");
 
 // iniciar server
--- BackEnd/src/routes/auth.route.js
+++ BackEnd/src/routes/auth.route.js
@@ -1,17 +1,15 @@
 import Router from "express";
-import {verifySignUp} from "../middlewares";
-import * as authController from "../controllers/auth.controller"
-
+import { verifySignUp } from "../middlewares";
+import * as authController from "../controllers/auth.controller";
 
 const router = new Router();
 
-router.post("/signup",[verifySignUp.validarEmailExiste,verifySignUp.verificarRolExiste],
-    authController.singUp
+router.post(
+  "/signup",
+  [verifySignUp.validarEmailExiste, verifySignUp.verificarRolExiste],
+  authController.singUp
 );
 
-router.get("/signin",
-    authController.signIn
-);
-
+router.get("/signin", authController.signIn);
 
 export default router;
